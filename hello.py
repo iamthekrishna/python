2d285b0e57d2be80fa60ca2877d502e065f47f95
Mayank Patel <mayankpatel.ahm@gmail.com>
2024-06-01 07:30:40 +0530

    Initial hello python file

---
+++ hello.py
@@ -0,0 +1,17 @@
+a=30
+b="Pooja"
+c=45.32
+d=True
+e=None
+#printing the variable
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+#printing type of variable
+print(type (a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
